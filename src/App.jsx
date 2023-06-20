ed610c73c659e8075f7d9f6e568970d4f1519dc3
nelson <peternelson063@gmail.com>
2023-06-20 23:03:34 +0100

    minor bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import ForgotPassword from "./pages/ForgotPassword";
-import Offers from "./pages/Offers";
-import Profile from "./pages/Profile";
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
-import Header from "./components/Header";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import PrivateRoute from "./components/PrivateRoute";
-import CreateListing from "./pages/CreateListing";
-import EditListing from "./pages/EditListing";
-import Listing from "./pages/Listing";
-import Category from "./pages/Category";
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Header from './components/Header';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import PrivateRoute from './components/PrivateRoute';
+import CreateListing from './pages/CreateListing';
+import EditListing from './pages/EditListing';
+import Listing from './pages/Listing';
+import Category from './pages/Category';
 
 function App() {
   return (
@@ -20,26 +20,29 @@ function App() {
       <Router>
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<PrivateRoute />}>
-            <Route path="/profile" element={<Profile />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/profile' element={<PrivateRoute />}>
+            <Route path='/profile' element={<Profile />} />
           </Route>
-          <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/offers" element={<Offers />} />
-          <Route path="/category/:categoryName" element={<Category />} />
-          <Route path="/category/:categoryName/:listingId" element={<Listing/>} />
-          <Route path="/create-listing" element={<PrivateRoute />}>
-            <Route path="/create-listing" element={<CreateListing />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/category/:categoryName' element={<Category />} />
+          <Route
+            path='/category/:categoryName/:listingId'
+            element={<Listing />}
+          />
+          <Route path='/create-listing' element={<PrivateRoute />}>
+            <Route path='/create-listing' element={<CreateListing />} />
           </Route>
-          <Route path="/edit-listing" element={<PrivateRoute />}>
-            <Route path="/edit-listing/:listingId" element={<EditListing />} />
+          <Route path='/edit-listing' element={<PrivateRoute />}>
+            <Route path='/edit-listing/:listingId' element={<EditListing />} />
           </Route>
         </Routes>
       </Router>
       <ToastContainer
-        position="bottom-center"
+        position='bottom-center'
         autoClose={5000}
         hideProgressBar={false}
         newestOnTop={false}
@@ -48,7 +51,7 @@ function App() {
         pauseOnFocusLoss
         draggable
         pauseOnHover
-        theme="dark"
+        theme='dark'
       />
     </>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,18 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBjd48zvRFZFBZ0BDTprLVrAVdTq_yx0ZU",
-  authDomain: "realsand-ng.firebaseapp.com",
-  projectId: "realsand-ng",
-  storageBucket: "realsand-ng.appspot.com",
-  messagingSenderId: "441221300888",
-  appId: "1:441221300888:web:116c43c57b8880635b06ed",
+  apiKey: 'AIzaSyBjd48zvRFZFBZ0BDTprLVrAVdTq_yx0ZU',
+  authDomain: 'realsand-ng.firebaseapp.com',
+  projectId: 'realsand-ng',
+  storageBucket: 'realsand-ng.appspot.com',
+  messagingSenderId: '441221300888',
+  appId: '1:441221300888:web:116c43c57b8880635b06ed',
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth()
+const auth = getAuth();
 const db = getFirestore(app);
 
 export { db, auth };
